f70a11c84961ce21a022cb832f05a9461a10e1c6
James Gawn <james@gawn.uk>
2023-12-02 23:47:31 +0000

    Completed day 2 question 1

--- day2/main.go
+++ day2/main.go
@@ -11,9 +11,38 @@ import (
 
 type game struct {
 	gameId int
-	// redCubes   int
-	// blueCubes  int
-	// greenCubes int
+	cubes  []cubeSet
+}
+
+type cubeSet struct {
+	redCubes   int
+	blueCubes  int
+	greenCubes int
+}
+
+func (game game) CalculateTotalCubes() cubeSet {
+	var cubes cubeSet
+
+	for _, currentSet := range game.cubes {
+		cubes.redCubes += currentSet.redCubes
+		cubes.greenCubes += currentSet.greenCubes
+		cubes.blueCubes += currentSet.blueCubes
+	}
+
+	return cubes
+}
+
+func (game game) IsValidGame(maxRedCubes int, maxBlueCubes int, maxGreenCubes int) bool {
+
+	for _, set := range game.cubes {
+		if set.redCubes > maxRedCubes || set.blueCubes > maxBlueCubes || set.greenCubes > maxGreenCubes {
+			return false
+		}
+	}
+
+	return true
+
+	//return game.CalculateTotalCubes().blueCubes <= maxBlueCubes && game.CalculateTotalCubes().greenCubes <= maxGreenCubes && game.CalculateTotalCubes().redCubes <= maxRedCubes
 }
 
 func ExecuteSolution(input io.Reader) int {
@@ -24,14 +53,34 @@ func ExecuteSolution(input io.Reader) int {
 		log.Debug().Msg("Parsed text: " + text)
 
 		game := ParseGame(text)
-		log.Debug().Interface("dict", game)
+		LogGame(game)
+
+		if game.IsValidGame(12, 14, 13) {
+			total += game.gameId
+		}
+
 	}
 	return total
 }
 
 func ParseGame(text string) game {
 	gameId := ParseGameNumber(text)
-	return game{gameId: gameId}
+
+	gameNumberIndex := strings.Index(text, ":")
+	pullsText := text[gameNumberIndex+1:]
+
+	cubeSets := ExtractCubePulls(pullsText)
+
+	return game{gameId: gameId, cubes: cubeSets}
+}
+
+func LogGame(game game) {
+	log.Debug().Int("gameId", game.gameId).
+		Int("redCubes", game.CalculateTotalCubes().blueCubes).
+		Int("blueCubes", game.CalculateTotalCubes().blueCubes).
+		Int("greenCubes", game.CalculateTotalCubes().greenCubes).
+		Bool("valid", game.IsValidGame(12, 14, 13)).
+		Send()
 }
 
 func ParseGameNumber(text string) int {
@@ -39,4 +88,33 @@ func ParseGameNumber(text string) int {
 	gameNumberString := text[5:gameNumberIndex]
 	gameNumber, _ := strconv.Atoi(gameNumberString)
 	return gameNumber
+}
+
+func ExtractCubePulls(pullText string) []cubeSet {
+	cubePulls := make([]cubeSet, 0)
+
+	pulls := strings.Split(pullText, ";")
+
+	for _, pull := range pulls {
+		var set cubeSet
+
+		pull := strings.Split(pull, ",")
+
+		for _, cubeColour := range pull {
+			trimmed := strings.Trim(cubeColour, " ")
+			split := strings.Split(trimmed, " ")
+			switch split[1] {
+			case "red":
+				set.redCubes, _ = strconv.Atoi(split[0])
+			case "green":
+				set.greenCubes, _ = strconv.Atoi(split[0])
+			case "blue":
+				set.blueCubes, _ = strconv.Atoi(split[0])
+			}
+		}
+
+		cubePulls = append(cubePulls, set)
+	}
+
+	return cubePulls
 }
--- day2/main_test.go
+++ day2/main_test.go
@@ -1,15 +1,16 @@
 package day2
 
 import (
+	"aoc2023/pkg/test"
 	"aoc2023/pkg/utils"
 	"testing"
 )
 
-// func TestValidateWithExample1(t *testing.T) {
-// 	file := utils.LoadFile("./input_test.txt")
-// 	result := ExecuteSolution(file)
-// 	test.EqualsInt(t, 8, result)
-// }
+func TestValidateWithExample1(t *testing.T) {
+	file := utils.LoadFile("./input_test.txt")
+	result := ExecuteSolution(file)
+	test.EqualsInt(t, 8, result)
+}
 
 // func TestValidateWithExample2(t *testing.T) {
 // 	file := utils.LoadFile("./input_test2.txt")
@@ -17,7 +18,19 @@ import (
 // 	test.EqualsInt(t, 281, result)
 // }
 
-func TestWorksWithRealFile(t *testing.T) {
-	file := utils.LoadFile("./input.txt")
-	ExecuteSolution(file)
+// func TestWorksWithRealFile(t *testing.T) {
+// 	file := utils.LoadFile("./input.txt")
+// 	ExecuteSolution(file)
+// }
+
+func TestIsValidCheckWhenValid(t *testing.T) {
+	set := cubeSet{redCubes: 12, blueCubes: 14, greenCubes: 13}
+	game := game{gameId: 1, cubes: []cubeSet{set}}
+	test.EqualsBool(t, true, game.IsValidGame(12, 14, 13))
+}
+
+func TestIsValidCheckWhenInvalid(t *testing.T) {
+	set := cubeSet{redCubes: 13, blueCubes: 15, greenCubes: 14}
+	game := game{gameId: 1, cubes: []cubeSet{set}}
+	test.EqualsBool(t, false, game.IsValidGame(12, 14, 13))
 }
