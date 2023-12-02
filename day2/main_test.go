7026af3c538ee0eefc5c01ed1d3110fe45d4b6f6
James Gawn <james@gawn.uk>
2023-12-02 22:32:29 +0000

    Switch logging library

--- day2/main_test.go
+++ day2/main_test.go
@@ -1,16 +1,15 @@
 package day2
 
 import (
-	"aoc2023/pkg/test"
 	"aoc2023/pkg/utils"
 	"testing"
 )
 
-func TestValidateWithExample1(t *testing.T) {
-	file := utils.LoadFile("./input_test.txt")
-	result := ExecuteSolution(file)
-	test.EqualsInt(t, 8, result)
-}
+// func TestValidateWithExample1(t *testing.T) {
+// 	file := utils.LoadFile("./input_test.txt")
+// 	result := ExecuteSolution(file)
+// 	test.EqualsInt(t, 8, result)
+// }
 
 // func TestValidateWithExample2(t *testing.T) {
 // 	file := utils.LoadFile("./input_test2.txt")
